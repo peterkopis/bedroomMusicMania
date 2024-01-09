2770c70742459800fb3cb84092d14c6667745b74
peter-pis <peter.pis@pcmancha.com>
2024-01-09 10:40:34 +0100

    add to register form password confirm field

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -65,6 +66,13 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('plainPassword',RepeatedType::class,[
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+                'invalid_message' => 'The password fields must match.',
+                ])
             ->add('artist_name',TextType::class,[
                 'constraints' => [
                     new NotBlank([
