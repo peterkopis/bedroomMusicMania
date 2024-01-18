be031e5c99d01d5bb512d930469057c10019ec2b
peter-pis <peter.pis@pcmancha.com>
2024-01-18 09:49:56 +0100

    button for play music  interface

--- public/js/acciones.js
+++ public/js/acciones.js
@@ -17,4 +17,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
     btnToggler.addEventListener('click', function(){
         toggleForm(uploadForm, btnToggler);
     });
+
+    //toggle between music play and stop
+
+    let buttons = document.getElementsByClassName('playPauseButton');
+    let iconos = document.getElementsByClassName('playPauseImage');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(){
+            if (iconos[i].src.endsWith('35_Music_Player_icon-icons.com_61459.png')) {
+                iconos[i].src = "../images/iconfinder-pause-stop-button-player-music-4593160_122283.png";
+            } else {
+                iconos[i].src = "../images/35_Music_Player_icon-icons.com_61459.png";
+            }
+        });
+    };
+    /*document.getElementById('playPauseButton').addEventListener('click', function() {
+        var image = document.getElementById('playPauseImage');
+        if (image.src.endsWith('35_Music_Player_icon-icons.com_61459.png')) {
+            image.src = "../images/iconfinder-pause-stop-button-player-music-4593160_122283.png";
+        } else {
+            image.src = "../images/35_Music_Player_icon-icons.com_61459.png";
+        }
+        });*/
 });
